77a0a20370f57dc4a818f088bf79bda2095ff102
Luis Jaimes <ferbluis143@gmail.com>
2023-10-17 22:38:47 -0300

    Add multiple products using array data

--- productManager.js
+++ productManager.js
@@ -46,52 +46,61 @@ class ProductManager {
   }
 }
 
+const productsToAdd = [
+  {
+    title: "Pastel de fresa",
+    description: "Delicioso pastel de fresa",
+    price: 19.99,
+    thumbnail: "imagen.jpg",
+    code: "STRAW456",
+    stock: 15,
+  },
+  {
+    title: "Galletas de chocolate",
+    description: "Crunchy cookies",
+    price: 9.99,
+    thumbnail: "cookie.jpg",
+    code: "CHOCO123",
+    stock: 30,
+  },
+  {
+    title: "Cheesecake",
+    description: "Delicioso pastel de queso",
+    price: 24.99,
+    thumbnail: "cheesecake.jpg",
+    code: "CHEESE789",
+    stock: 10,
+  },
+  {
+    title: "Red Velvet Cake",
+    description: "Red velvet cake with cream cheese frosting",
+    price: 29.99,
+    thumbnail: "redvelvet.jpg",
+    code: "VELVET321",
+    stock: 12,
+  },
+  {
+    title: "Brownie",
+    description: "Rich chocolate brownie with nuts",
+    price: 12.99,
+    thumbnail: "brownie.jpg",
+    code: "BROWN456",
+    stock: 20,
+  },
+];
+
 const manager = new ProductManager();
 
-manager.addProduct(
-  "Pastel de fresa",
-  "Delicioso pastel de fresa",
-  19.99,
-  "imagen.jpg",
-  "STRAW456",
-  15
-);
-
-manager.addProduct(
-  "Galletas de chocolate",
-  "Crunchy cookies",
-  9.99,
-  "cookie.jpg",
-  "CHOCO123",
-  30
-);
-
-manager.addProduct(
-  "Cheesecake",
-  "Delicioso pastel de queso",
-  24.99,
-  "cheesecake.jpg",
-  "CHEESE789",
-  10
-);
-
-manager.addProduct(
-  "Red Velvet Cake",
-  "Red velvet cake with cream cheese frosting",
-  29.99,
-  "redvelvet.jpg",
-  "VELVET321",
-  12
-);
-
-manager.addProduct(
-  "Brownie",
-  "Rich chocolate brownie with nuts",
-  12.99,
-  "brownie.jpg",
-  "BROWN456",
-  20
-);
+for (const productData of productsToAdd) {
+  manager.addProduct(
+    productData.title,
+    productData.description,
+    productData.price,
+    productData.thumbnail,
+    productData.code,
+    productData.stock
+  );
+}
 
 console.log("Todos los productos:");
 console.log(manager.getProducts());
